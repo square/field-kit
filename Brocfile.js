97cd5f719a7e051249ccb0588d6f855d641ff8f4
Brian Donovan <donovan@squareup.com>
2014-03-28 12:06:08 -0700

    Use ES6 classes.

--- Brocfile.js
+++ Brocfile.js
@@ -1,13 +1,26 @@
 var browserify = require('broccoli-browserify');
 var concat = require('broccoli-concat');
 var ENV = process.env.ENVIRONMENT || 'development';
+var es6class = require('es6-class');
+var Filter = require('broccoli-filter');
+
+function es6(inputTree) {
+  var es6classFilter = new Filter(inputTree, {
+    extensions: ['js'],
+    targetExtensions: ['js']
+  });
+  es6classFilter.processString = function(source) {
+    return es6class.compile(source);
+  };
+  return es6classFilter;
+}
 
 module.exports = function(broccoli) {
   var lib = broccoli.makeTree('lib');
   var test = broccoli.makeTree('test');
   var qunit = broccoli.makeTree('node_modules/qunit/support/qunit/qunit');
 
-  var fieldKit = browserify(lib, {
+  var fieldKit = browserify(es6(lib), {
     entries: ['./index'],
     outputFile: '/field-kit.js',
     bundle: { debug: false, standalone: 'FieldKit' }
@@ -25,9 +38,9 @@ module.exports = function(broccoli) {
 
   switch (ENV) {
     case 'development':
-      return [fieldKit, testSetup, allTests, test, qunit];
+      return [es6(fieldKit), es6(testSetup), allTests, test, qunit];
 
     case 'production':
-      return [fieldKit];
+      return [es6(fieldKit)];
   }
 };
