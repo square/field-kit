c22ecb50b2720dc34f7e79c3eb022826837def64
Alex Gorbatchev <agorbatchev@squareup.com>
2015-08-11 11:18:16 -0700

    Sets up `karma.conf.js`.

--- karma.conf.js
+++ karma.conf.js
@@ -1,66 +1,27 @@
-// Karma configuration
-// Generated on Mon May 19 2014 13:30:32 GMT-0700 (PDT)
-
 module.exports = function(config) {
   config.set({
+    basePath   : '',
+    frameworks : ['browserify', 'mocha', 'chai'],
+    reporters  : ['mocha'],
+    port       : process.env.KARMA_PORT || 2000,
+    browsers   : ['Chrome'],
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
-
-
-    // list of files / patterns to load in the browser
     files: [
-      'node_modules/sinon/pkg/sinon.js',
-      'node_modules/string-selection/dist/string-selection.js',
-      'node_modules/keysim/dist/keysim.js',
-      'dist/field-kit.js',
-      'build/test/all.js'
+      'src/index.js',
+      'test/unit/**/*_test.js'
     ],
 
+    preprocessors: {
+      'src/**/*.js': ['eslint', 'browserify'],
+      'test/unit/**/*.js': ['browserify']
+    },
 
-    // list of files to exclude
-    exclude: [],
+    eslint: {
+      stopOnError: true
+    },
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    browserify: {
+      debug: true
+    }
   });
 };
