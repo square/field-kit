096dc64db35af78dbcdddbbdee1ae7126315f7f6
Brian Donovan <donovan@squareup.com>
2015-09-10 14:10:44 -0700

    Remove named functions since they aren't configurable.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,8 +6,12 @@ import rimraf from 'rimraf';
 import uglify from 'gulp-uglify';
 import { rollup } from 'rollup-babel';
 
-function dist() {
-  return rollup({
+gulp.task('clean:lib', done => rimraf('./lib', done));
+gulp.task('clean:dist', done => rimraf('./dist', done));
+
+gulp.task('dist', ['dist:not-minified', 'dist:minified']);
+gulp.task('dist:not-minified', ['clean:dist'], () =>
+  rollup({
     entry: 'src/index.js',
     babel: { loose: ['es6.spread', 'es6.parameters', 'es6.destructuring'] }
   }).then(result =>
@@ -16,28 +20,17 @@ function dist() {
       format: 'umd',
       moduleName: 'FieldKit'
     })
-  );
-}
-
-function minify() {
-  return gulp.src('dist/field-kit.js')
+  ));
+gulp.task('dist:minified', ['dist:not-minified'], () =>
+  gulp.src('dist/field-kit.js')
     .pipe(concat('field-kit.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
-}
+    .pipe(gulp.dest('dist')));
 
-gulp.task('clean:lib', done => rimraf('./lib', done));
-gulp.task('clean:dist', done => rimraf('./dist', done));
-
-gulp.task('dist', ['dist:not-minified', 'dist:minified']);
-gulp.task('dist:not-minified', ['clean:dist'], dist);
-gulp.task('dist:minified', ['dist:not-minified'], minify);
-
-gulp.task('gh-pages', function () {
-  return gulp.src(['**/*', '!node_modules/**'])
+gulp.task('gh-pages', () =>
+  gulp.src(['**/*', '!node_modules/**'])
     .pipe(print())
-    .pipe(ghPages());
-});
+    .pipe(ghPages()));
 
 gulp.task('build', ['dist']);
 gulp.task('default', ['build']);
